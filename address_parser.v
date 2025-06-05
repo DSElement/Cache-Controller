60c8a5127b24cef9509b40c387c3b8a80dc60202
Ghincul Dan <158288362+DSElement@users.noreply.github.com>
2025-06-05 21:14:43 +0300

    Update address_parser.v with byte adressed principle

--- address_parser.v
+++ address_parser.v
@@ -1,14 +1,17 @@
 module address_parser (
     input  [31:0] addr,
-    output [20:0] tag,
+    output [18:0] tag,
     output [6:0]  index,
-    output [3:0]  block_offset
+    output [3:0]  block_offset,
+    output [1:0]  word_offset
 );
 
-assign tag = addr[31:11];
+    assign tag = addr[31:13];
 
-assign index = addr[10:4];
+    assign index = addr[12:6]; //7 bits because we have 128 blocks
 
-assign block_offset = addr[3:0];
+    assign block_offset = addr[5:2]; //4 bits because we have 16 words
+
+    assign word_offset = addr[1:0];  //2 bits because we have 4 bytes/word
 
 endmodule
